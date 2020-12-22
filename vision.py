3307ee5e32911417248324346f4224b99ad8984b
ssaulrj <saul.reyna.juarez@gmail.com>
2020-12-21 23:18:06 -0600

    Todo el proyecto 2

--- vision.py
+++ vision.py
@@ -48,3 +48,6 @@ class Avision:
 		point1 = rs.rs2_deproject_pixel_to_point(self.color_intrin, [ix1, iy1], iz1)
 		point2 = rs.rs2_deproject_pixel_to_point(self.color_intrin, [ix2, iy2], iz2)
 		return math.sqrt(math.pow(point1[0] - point2[0], 2) + math.pow(point1[1] - point2[1], 2) + math.pow(point1[2] - point2[2], 2))
+
+	def x(self):
+		pass
